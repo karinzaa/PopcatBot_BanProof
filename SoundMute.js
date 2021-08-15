a94b005f0370047c106f4a221a3538d90b11924f
Karinzaa Vitoonkijwanit <25452435+karinzaa@users.noreply.github.com>
2021-08-16 01:15:30 +0700

    Add Note

--- SoundMute.js
+++ SoundMute.js
@@ -2,8 +2,9 @@
 ส่วนนี้เอาไว้ปิดเสียง pops ที่น่ารำคาญ
 1) เปิด console (F12 or CTRL+SHIFT+I)
 2) เอา code ใส่แล้ว enter
-*/
 
+Note: ต้องรัน code bot ก่อนใช้ตัวปิดเสียง pops นะ
+*/
 document.querySelector("#app").__vue__.pop_howls[0] = new
 Howl({src:"data:audio/ogg;base64,null"})
 document.querySelector("#app").__vue__.pop_howls[1] = new
