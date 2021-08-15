530f11042eaac3128e6e8ee2fdfa264fb4a32efd
Karinzaa Vitoonkijwanit <25452435+karinzaa@users.noreply.github.com>
2021-08-15 21:31:34 +0700

    Add SoundMute.js

---
+++ SoundMute.js
@@ -0,0 +1,14 @@
+/*
+ส่วนนี้เอาไว้ปิดเสียง pops ที่น่ารำคาญ
+1) เปิด console (F12 or CTRL+SHIFT+I)
+2) เอา code ใส่แล้ว enter
+*/
+
+document.querySelector("#app").__vue__.pop_howls[0] = new
+Howl({src:"data:audio/ogg;base64,null"})
+document.querySelector("#app").__vue__.pop_howls[1] = new
+Howl({src:"data:audio/ogg;base64,null"})
+document.querySelector("#app").__vue__.pop_howls[2] = new
+Howl({src:"data:audio/ogg;base64,null"})
+document.querySelector("#app").__vue__.pop_howls[3] = new
+Howl({src:"data:audio/ogg;base64,null"})
